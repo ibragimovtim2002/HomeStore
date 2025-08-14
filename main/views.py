d36cac6966f636072a0fdacf0c46faea7aa89664
Timur Ibragimov <ibragimov.tim02@gmail.com>
2025-08-14 17:40:02 +0300

    Update index.html, add static files

--- main/views.py
+++ main/views.py
@@ -3,13 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context = {
-        'title': 'Home',
-        'content': 'Главная страница',
-        'list': ['first', 'second', 'third'],
-        'is_authenticated': True
-    }
-    return render(request, 'main/index.html', context)
+
+    return render(request, 'main/index.html')
 
 def about(request):
     return HttpResponse("About")
