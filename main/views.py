e87c9574365af6c88c63fb643df27fc528d76b87
Timur Ibragimov <ibragimov.tim02@gmail.com>
2025-08-14 18:23:05 +0300

    Create about.html

--- main/views.py
+++ main/views.py
@@ -10,4 +10,9 @@ def index(request):
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse("About")
+    context= {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Текст о нас'
+    }
+    return render(request, 'main/about.html', context)
